0eb9acacdd0b4ac70d8428ad7a45ed8729ec2a9b
kmartynov200120012001-cpu <kmartynov200120012001@gmail.com>
2026-07-02 00:26:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -322,9 +322,9 @@ def get_paid_profile_text(user: dict) -> str:
             text = (
                 f"🟢 <b>VPN работает</b>\n\n"
                 f"<blockquote><b>Активен до:</b>\n"
-                f"<a>{end_date_full}</a></blockquote>\n\n"
+                f"<i>{end_date_full}</i></blockquote>\n\n"
                 f"💎 Продлить доступ можно в любой момент\n\n"
-                f"🔑 <b>Ваш ключ доступа:</b>\n"
+                f"🔑 <b>Ваш VPN-ключ:</b>\n"
                 f"<blockquote><code>{key_link}</code></blockquote>"
             )
         else:
@@ -333,7 +333,7 @@ def get_paid_profile_text(user: dict) -> str:
                 f"🟡 <b>VPN работает</b>\n\n"
                 f"<blockquote>⏳ <b>Подписка истекает через:</b>\n"
                 f"<i>{time_left_text}</i></blockquote>\n\n"
-                f"💎 Продлите заранее, чтобы не потерять доступ.\n\n"
+                f"💎 Продлите заранее, чтобы не потерять доступ\n\n"
                 f"🔑 <b>Ваш VPN-ключ:</b>\n"
                 f"<blockquote><code>{key_link}</code></blockquote>"
             )
